eaefc973afde61271064cf301d3fb1d38d689a02
AmerDawoood <amermadawood@gmail.com>
2023-09-21 16:49:56 +0300

    update api route

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::get('auth/access-tokens', [AccessTokensController::class , 'index']);
+        Route::delete('auth/access-tokens/{id}', [AccessTokensController::class , 'destroy']);
+        Route::apiResource('/classrooms', ClassroomController::class);
+        Route::apiResource('classrooms.classworks', ClassworkController::class);
+    });
+
+
+    Route::middleware('guest:sanctum')->group(function () {
+        Route::post('auth/access-tokens', [AccessTokensController::class , 'store']);
+    });
+
 });
